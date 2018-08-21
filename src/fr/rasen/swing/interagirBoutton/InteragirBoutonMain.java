648c0f76f488cd62b1492ddc716131442b7c8c26
sebastien <sebastien.lebail97@gmail.com>
2018-08-21 18:32:43 +0200

    Interagir avec son Bouton complet.

---
+++ src/fr/rasen/swing/interagirBoutton/InteragirBoutonMain.java
@@ -0,0 +1,12 @@
+package fr.rasen.swing.interagirBoutton;
+
+import fr.rasen.swing.positionnerBoutton.Fenetre;
+
+public class InteragirBoutonMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fenetre f = new Fenetre();
+	}
+
+}
--- src/fr/rasen/swing/positionnerBoutton/Fenetre.java
+++ src/fr/rasen/swing/positionnerBoutton/Fenetre.java
@@ -2,32 +2,52 @@ package fr.rasen.swing.positionnerBoutton;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import fr.rasen.swing.premiereFenetre.Panneau;
 
-public class Fenetre extends JFrame{
+public class Fenetre extends JFrame implements ActionListener{
 
 	private Panneau pan = new Panneau();
-	private JButton bouton = new JButton("Mon Boutton");
-	JPanel container = new JPanel();
+	private JButton bouton = new JButton("bouton 1");
+	private JButton bouton2 = new JButton("bouton 2");
+	private JPanel container = new JPanel();
+	private JLabel label = new JLabel("Le JLabel");
+	private int compteur = 0;
 
 	public Fenetre() {
 		this.setTitle("Animation");
 		this.setSize(300, 300);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
+
 		container.setBackground(Color.white);
 		container.setLayout(new BorderLayout());
 		container.add(pan, BorderLayout.CENTER);
-		container.add(bouton, BorderLayout.SOUTH);
+
+		bouton.addActionListener(this);
+		bouton2.addActionListener(this);
+
+		JPanel south = new JPanel();
+		south.add(bouton);
+		south.add(bouton2);
+		container.add(south, BorderLayout.SOUTH);
+
+		Font police = new Font("Tahoma", Font.BOLD, 16);
+		label.setFont(police);
+		label.setForeground(Color.blue);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		container.add(label, BorderLayout.NORTH);
 		this.setContentPane(container);
-		this.setVisible(true);    
-		go();
-	}
+		this.setVisible(true);
+		go();	}
 
 	private void go(){  
 		int x = pan.getPosX(), y = pan.getPosY();
@@ -63,5 +83,15 @@ public class Fenetre extends JFrame{
 				e.printStackTrace();
 			}
 		} 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if(arg0.getSource() == bouton)
+		    label.setText("Vous avez cliqué sur le bouton 1");
+
+		  if(arg0.getSource() == bouton2)
+		    label.setText("Vous avez cliqué sur le bouton 2");
 	}
 }
